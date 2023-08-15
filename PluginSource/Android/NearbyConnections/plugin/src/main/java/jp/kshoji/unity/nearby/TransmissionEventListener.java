1b15bc939deeb4dea11f03f4eecbd26407ba00a3
Kaoru Shoji <0x0badc0de@gmail.com>
2023-08-16 06:05:30 +0900

    Add stream transfer feature for Android

--- PluginSource/Android/NearbyConnections/plugin/src/main/java/jp/kshoji/unity/nearby/TransmissionEventListener.java
+++ PluginSource/Android/NearbyConnections/plugin/src/main/java/jp/kshoji/unity/nearby/TransmissionEventListener.java
@@ -6,4 +6,8 @@ public interface TransmissionEventListener {
     void onFileTransferUpdate(String endpointId, long payloadId, long bytesTransferred, long totalSize);
     void onFileTransferFailed(String endpointId, long payloadId);
     void onFileTransferCancelled(String endpointId, long payloadId);
+    void onReceiveStream(String endpointId, long id, byte[] payload);
+    void onStreamTransferComplete(String endpointId, long id);
+    void onStreamTransferFailed(String endpointId, long payloadId);
+    void onStreamTransferCancelled(String endpointId, long payloadId);
 }
